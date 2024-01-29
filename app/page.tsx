47de995a7dec5f9c063212aa99b431dca2fc62e8
beishenbaeva <dianabeishenbaeva@MacBook-Air-Diana.local>
2024-01-29 19:48:08 +0600

    fixed bug during deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Navbar from '@/app/ui/dashboard/navbar'
-import DashboardSkeleton from './ui/skeletons';
-
-
 export default function Page() {
-  return (
-    <div>
-        <Navbar/>
-    </div>
-  );
+  return <div>dashboard</div>;
 }
--- app/ui/dashboard/navbar.tsx
+++ app/ui/dashboard/navbar.tsx
@@ -8,44 +8,42 @@ import { montserrat } from '@/app/ui/fonts';
 export default function Navbar() {
   return (
     <header
-      className={`${montserrat.className} loaded-155 h-100 container mx-auto flex items-end justify-between p-9 py-2  font-light`}
+      className={`${montserrat.className} loaded-155 container mx-auto flex flex-wrap items-end justify-between p-4 py-2 font-light sm:p-9`}
     >
       {/* Логотип */}
-      <div className="nav-logo w-200 h-200  bg-f5">
+      <div className="mb-4 w-full bg-f5 sm:mb-0 sm:w-1/4">
         <Link href={'/dashboard/home'}>
-          {' '}
           <Image src="/logojayjay.png" alt="" width={124} height={50} />
         </Link>
       </div>
 
       {/* Навигационное меню */}
-      <nav className="nav-list w-1/3">
-        <ul className="ml-[10px] flex w-full justify-between">
-          <li>
+      <nav className="mb-4 w-full sm:mb-0 sm:w-1/2">
+        <ul className="flex flex-wrap sm:justify-between">
+          <li className="mb-2 sm:mb-0 sm:mr-4">
             <Link href={'/dashboard/guarantee'}>Гарантия</Link>
           </li>
-          <li>
+          <li className="mb-2 sm:mb-0 sm:mr-4">
             <Link href={'/dashboard/delivery'}>Доставка и оплата</Link>
           </li>
-          <li>
+          <li className="mb-2 sm:mb-0">
             <Link href={'/dashboard/location'}>Магазин</Link>
           </li>
         </ul>
       </nav>
 
       {/* Корзина */}
-      <div className="cart   mt-2 text-gray-500">
-        <Image src={'/cart.png'} height={30} width={30} alt=""></Image>
+      <div className="cart text-gray-500">
+        <Image src={'/cart.png'} height={30} width={30} alt="" />
       </div>
 
       {/* Контактный номер */}
-      <aside className="number">
+      <aside className="number mt-4 sm:mt-0">
         <ul>
           <li className={styles.fontNum}>+996 555 73 27 57</li>
-          <li className="mr-4 text-sm">связаться по телефону</li>
+          <li className="text-sm">связаться по телефону</li>
         </ul>
       </aside>
     </header>
   );
 }
-// className = '';
